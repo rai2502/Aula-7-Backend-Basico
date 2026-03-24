53197ef7190cdc06e8b0cce29e95396886dfed5b
Raí Costa <rai.c@aluno.senai.br>
2026-03-24 09:12:27 -0300

    Add files via upload

--- data/supabase.js
+++ data/supabase.js
@@ -1,10 +1,10 @@
-const { createClient } = require('@supabase/supabase-js');
+const { createClient} = require('@supabase/supabase-js');
 require('dotenv').config();
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_KEY;
 
-if(!supabaseUrl || !supabaseKey){
-    console.error('SUPABASE_URL ou SUPABASE_KEY não foi encontrado no .env')
+if (!supabaseUrl || !supabaseKey) {
+    console.error('SUPABASE_URL ou SUPABASE_KEY não foi encontrado no .env');
 }
 
 const supabase = createClient(supabaseUrl, supabaseKey);
